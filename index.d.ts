5337439d7a8d569c6f1547a51312aa99b4c850c0
Jsman <VuReact@qq.com>
2020-08-12 15:26:59 +0800

    1.0.7 新增链式调用的方式,监视数据的函数

--- index.d.ts
+++ index.d.ts
@@ -1,31 +1,28 @@
-declare function isEvent(val: number | string): boolean;
-declare function isOdd(val: number | string): boolean;
-declare function isEmpty(val: string): boolean;
-declare function isNumber(val: number): boolean;
-declare function isUrl(val: string): boolean;
-declare function allEqual(arr: Array<any>): boolean;
-declare function bifurcateBy(arr: Array<any>, fn: Function): Array<any>;
-declare function capitalizeEveryWord(str: string): string;
-declare function compact(arr: Array<any>): Array<any>;
-declare function defaults(obj: object, ...defs: any): object;
-declare function Drop(arr: Array<any>, n: number = 1): Array<any>;
-declare function dropRightWhile(arr: Array<any>, func: Function): Array<any>;
-declare function dropWhile(arr: Array<any>, func: Function): Array<any>;
-declare function elementContains(
-  parent: HTMLElement,
-  child: HTMLElement
-): boolean;
-declare function getColonTimeFromDate(date: Date): String;
-declare function getDaysDiffBetweenDates(
-  dateInitial: number,
-  dateFinal: number
-): number;
-declare function getStyle(el: HTMLElement, ruleName: string | any): any;
-declare function getType(val: any): String;
-declare function hasClass(el: HTMLElement, className: string): boolean;
-declare function insertAfter(el: HTMLElement, htmlString: string): void;
-declare function insertBefore(el: HTMLElement, htmlString: string): void;
-declare function isAfterDate(dateA: Date, dateB: Date): boolean;
-declare function Last(arr: Array<any>): Array<any>;
-declare function isTrue(valOne: any, valTwo: any): boolean;
-declare function There(arr: Array<any>, val: any): boolean;
+export declare class Verification {
+  isEvent(): this;
+  isOdd(): this;
+  isEmpty(): this;
+  isNumber(): this;
+  isUrl(): this;
+  allEqual(): this;
+  bifurcateBy(arr: Array<any>, fn: Function): Array<any>;
+  capitalizeEveryWord(str: string): string;
+  compact(arr: Array<any>): Array<any>;
+  defaults(obj: object, ...defs: any): object;
+  Drop(arr: Array<any>, n: number = 1): Array<any>;
+  dropRightWhile(arr: Array<any>, func: Function): Array<any>;
+  dropWhile(arr: Array<any>, func: Function): Array<any>;
+  elementContains(parent: HTMLElement, child: HTMLElement): this;
+  getColonTimeFromDate(date: Date): String;
+  getDaysDiffBetweenDates(dateInitial: number, dateFinal: number): number;
+  getStyle(el: HTMLElement, ruleName: string | any): any;
+  getType(val: any): String;
+  hasClass(el: HTMLElement, className: string): this;
+  insertAfter(el: HTMLElement, htmlString: string): this;
+  insertBefore(el: HTMLElement, htmlString: string): this;
+  isAfterDate(dateA: Date, dateB: Date): this;
+  Last(arr: Array<any>): Array<any>;
+  isTrue(valOne: any, valTwo: any): this;
+  There(arr: Array<any>, val: any): this;
+  Monitor(Obj: Array<string | number> | Object): Array<Object>;
+}
